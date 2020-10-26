63dd709857eb195a2cb6b5a77d32cfe370e0cfe2
George Corney <haxiomic@gmail.com>
2020-10-26 23:28:57 +0000

    [test] Improve 9934 regression test

--- tests/unit/src/unit/issues/Issue9934.hx
+++ tests/unit/src/unit/issues/Issue9934.hx
@@ -1,25 +1,52 @@
 package unit.issues;
 
 class Issue9934 extends unit.Test {
-
+	#if js
 	function test() {
-		try new ES6Extension('es6', 1) catch(e: Any) {
+		try {
+			var instance = new ES5Extension('es5', 1);
+			eq(instance.a, 'es5');
+			eq(instance.b, 1);
+			eq(instance.c, 2);
+			t(js.Syntax.instanceof(instance, Issue9934_ES5Class));
+
+			var instance = new ES6Extension('es6', 1);
+			eq(instance.a, 'es6');
+			eq(instance.b, 1);
+			eq(instance.c, 2);
+
+			t(js.Syntax.instanceof(instance, Issue9934_ES6Class));
+		} catch(e: Any) {
 			assert("Should not throw an exception when constructing an extended ES6 class");
 		}
 	}
 
-}
-
-@:native('Issue9934ES6Class')
-extern class Issue9934ES6Class {
-	var a: String;
-	var b: Int;
-	function new(a: String, b: Int);
 	private static function __init__():Void {
 		haxe.macro.Compiler.includeFile("unit/issues/Issue9934External.js");
 	}
-	
+	#end
 }
 
-class ES6Extension extends Issue9934ES6Class {
+#if js
+@:native('Issue9934_ES6Class')
+extern class Issue9934_ES6Class {
+	var a: String;
+	var b: Int;
+	function new(a: String, b: Int);
 }
+
+@:native('Issue9934_ES5Class')
+extern class Issue9934_ES5Class {
+	var a: String;
+	var b: Int;
+	function new(a: String, b: Int);	
+}
+
+class ES6Extension extends Issue9934_ES6Class {
+	public final c = 2;
+}
+
+class ES5Extension extends Issue9934_ES5Class {
+	public final c = 2;
+}
+#end
--- tests/unit/src/unit/issues/Issue9934External.js
+++ tests/unit/src/unit/issues/Issue9934External.js
@@ -1,7 +1,11 @@
-class Issue9934ES6Class {
+class Issue9934_ES6Class {
 	constructor(a, b) {
-		console.log('Constructing ES6Class with args:', a, b);
 		this.a = a;
 		this.b = b;
 	}
+}
+
+function Issue9934_ES5Class(a, b) {
+	this.a = a;
+	this.b = b;
 }
